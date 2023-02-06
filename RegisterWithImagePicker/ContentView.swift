a7d845d8f80822d6930445b43ee4b5baacad5eb4
adarsh <adarsh0ranjan@gmail.com>
2023-02-06 14:32:36 +0530

    final commit

---
+++ RegisterWithImagePicker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RegisterWithImagePicker
+//
+//  Created by Roro Solutions on 06/02/23.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
